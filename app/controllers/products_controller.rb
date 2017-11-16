fc9e5c99d40180f5f307bbe244fa92e23066a433
alejandro <ale.gonzalezn@alumnos.duoc.cl>
2017-11-16 23:37:38 +0000

    asdsdadsds

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
 
   # GET /products
   # GET /products.json
@@ -72,4 +73,7 @@ class ProductsController < ApplicationController
       params.require(:product).permit(:name, :price, :detail, :shop_id, :user_id, :product_img)
     end
     
+    def set_s3_direct_post
+      @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+    end
 end
--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,6 @@
 class ShopsController < ApplicationController
   before_action :set_shop, only: [:show, :edit, :update, :destroy]
+  before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
   
   # GET /shops
   # GET /shops.json
@@ -72,4 +73,7 @@ class ShopsController < ApplicationController
       params.require(:shop).permit(:name, :greeting, :number, :whatsapp, :phone, :user_id, :shop_img)
     end
     
+    def set_s3_direct_post
+      @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+    end    
 end
