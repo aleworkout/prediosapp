5248b7552a69bebff89ab92f0d65a4999e1f4ec1
alejandro <ale.gonzalezn@alumnos.duoc.cl>
2017-11-16 23:26:56 +0000

    sadsddds

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,5 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-  before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
 
   # GET /products
   # GET /products.json
@@ -73,7 +72,4 @@ class ProductsController < ApplicationController
       params.require(:product).permit(:name, :price, :detail, :shop_id, :user_id, :product_img)
     end
     
-    def set_s3_direct_post
-      @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
-    end
 end
